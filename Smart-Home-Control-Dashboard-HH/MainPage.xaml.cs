3455d563efbbce452958d1f22f554e0de147fa37
DavidHE <davidhoangem@gmail.com>
2026-01-05 10:54:27 +0700

    feat(layout): build main dashboard layout skeleton

--- Smart-Home-Control-Dashboard-HH/MainPage.xaml.cs
+++ Smart-Home-Control-Dashboard-HH/MainPage.xaml.cs
@@ -1,25 +1,9 @@
-﻿namespace Smart_Home_Control_Dashboard_HH
+﻿namespace Smart_Home_Control_Dashboard_HH;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public MainPage()
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        InitializeComponent();
     }
-
 }
